ba4101203f2fb5f1b3c5f66519a5bbb49cc29057
Clément de l'Hamaide <clemaez@hotmail.fr>
2016-04-15 00:40:30 +0200

    More work on the installer

--- install.sh
+++ install.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 #
-# Installation script for aeroPi
+# Installer script for aeroPi
 #
 # Author: Clément de l'Hamaide
 # Date: April 2016
 
 
 echo "Welcome to aeroPi installer"
-echo "Press a key to continue"
-read -n 1
+echo "###########################"
 
-echo "Updating RPi..."
-sudo apt-get dist-upgrade
-sudo apt-get update
-#sudo rpi-update
-sudo apt-get clean
+echo "raspi-config will be launched, then  and expand filesystem"
+echo "Then reboot the RPi"
+read -p "Continue the script? <Y/n> " prompt
+if [[ $prompt =~ [nN](o)* ]]
+then
+  exit 0
+fi
+
+echo "Setting hostname..."
+sudo echo "aeroPi" > /etc/hostname
+
+echo "Setting new password for Pi user..."
+passwd
 
 echo "Installing dependencies..."
 sudo apt-get install gpsd gpsd-clients python-gps
+
+echo "Update RPi..."
+echo "sudo apt-get update"
+echo "sudo apt-get dist-upgrade"
+echo "sudo apt-get clean"
+echo "Then reboot the RPi"
+read -p "Continue the script? <Y/n> " prompt
+if [[ $prompt =~ [nN](o)* ]]
+then
+  exit 0
+fi
+
+echo "sudo rpi-update"
+echo "Then reboot the RPi"
+read -p "Continue the script? <Y/n> " prompt
+if [[ $prompt =~ [nN](o)* ]]
+then
+  exit 0
+fi
 
 echo "Done"
