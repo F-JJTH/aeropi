58d0852b9018d4bccc445f255a1fa7b3832cfeb3
Clément de l'Hamaide <clemaez@hotmail.fr>
2016-04-21 21:21:58 +0000

    Implement websocket in multi threaded architecture

--- src/server.py
+++ src/server.py
@@ -1,87 +1,20 @@
 #!/usr/bin/env python3
-
-import sys, getopt
-
+import signal, sys
 sys.path.append('.')
 import RTIMU
 import os.path
 import time
 import math
 import gps3
-import threading
-import asyncio
 import websockets
-
-SETTINGS_FILE = "RTIMULib"
-exitFlag = 0
-
-#  computeHeight() - the conversion uses the formula:
-#
-#  h = (T0 / L0) * ((p / P0)**(-(R* * L0) / (g0 * M)) - 1)
-#
-#  where:
-#  h  = height above sea level
-#  T0 = standard temperature at sea level = 288.15
-#  L0 = standard temperatur elapse rate = -0.0065
-#  p  = measured pressure
-#  P0 = static pressure = 1013.25
-#  g0 = gravitational acceleration = 9.80665
-#  M  = mloecular mass of earth's air = 0.0289644
-#  R* = universal gas constant = 8.31432
-#
-#  Given the constants, this works out to:
-#
-#  h = 44330.8 * (1 - (p / P0)**0.190263)
-
-def computeHeight(pressure):
-  return 44330.8 * (1 - pow(pressure / 1013.25, 0.190263));
+import asyncio
+import threading
 
 
-class IMUThread(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    print("Using settings file " + SETTINGS_FILE + ".ini")
-    if not os.path.exists(SETTINGS_FILE + ".ini"):
-      print("Settings file does not exist, will be created")
 
-    self.s = RTIMU.Settings(SETTINGS_FILE)
-    self.imu = RTIMU.RTIMU(self.s)
-    self.pressure = RTIMU.RTPressure(self.s)
-
-    if (not self.imu.IMUInit()):
-      print("IMU Init Failed")
-      sys.exit(1)
-
-    self.imu.setSlerpPower(0.02)
-    self.imu.setGyroEnable(True)
-    self.imu.setAccelEnable(True)
-    self.imu.setCompassEnable(True)
-
-    if(not self.pressure.pressureInit()):
-      print("Pressure sensor Init Failed")
-      sys.exit(1)
-
-    self.poll_interval = self.imu.IMUGetPollInterval()
-    print("Recommended Poll Interval: %dmS\n" % self.poll_interval)
-
-  def run(self):
-    while True:
-      if self.imu.IMURead():
-        data = self.imu.getIMUData()
-        (data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"]) = self.pressure.pressureRead()
-        fusionPose = data["fusionPose"]
-        json = '{"imu":[{"pressureValid": "%i", "pressure": "%d", "temperatureValid": "%i", "temperature": "%d", "pitch": "%d", "roll": "%d", "yaw": "%d"}]}' % (data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"], math.degrees(fusionPose[0]), math.degrees(fusionPose[1]), math.degrees(fusionPose[2]))
-        print(json)
-        time.sleep(self.poll_interval*1.0/1000.0)
-
-
-class GPSThread(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    self.session = gps3.GPSDSocket(host='localhost')
-    self.fix = gps3.Fix()
-
-  def run(self):
+class GPSWebSocket(threading.Thread):    
+  @asyncio.coroutine
+  def handler(self, websocket, path):
     for new_data in self.session:
       if new_data:
         self.fix.refresh(new_data)
@@ -92,22 +25,72 @@ class GPSThread(threading.Thread):
           spd =  self.fix.TPV["speed"]
           hdg =  self.fix.TPV["track"]
           climb = self.fix.TPV['climb']
-          json = '{"gps":[{"lat": "%s", "lon": "%s", "hdg": "%f", "climb": "%f", "alt": "%f", "spd": "%f"}]}' % (lat, lon, float(hdg), climb, alt, spd)
-          print(json)
+          stream = '{"gps":[{"lat": "%s", "lon": "%s", "hdg": "%f", "climb": "%f", "alt": "%f", "spd": "%f"}]}'\
+                   % (lat, lon, float(hdg), climb, alt, spd)
+          yield from websocket.send(stream)
         except ValueError:
           pass
+
+  def run(self):
+    self.session = gps3.GPSDSocket(host='localhost')
+    self.fix = gps3.Fix()
+    
+    start_server = websockets.serve(self.handler, '127.0.0.1', 7001)
+    eventloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(eventloop)
+    eventloop.run_until_complete(start_server)
+    eventloop.run_forever()
+
+
+
+class IMUWebSocket(threading.Thread):    
+  @asyncio.coroutine
+  def handler(self, websocket, path):
+    while True:
+      if self.imu.IMURead():
+        data = self.imu.getIMUData()
+        (data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"]) = self.pressure.pressureRead()
+        fusionPose = data["fusionPose"]
+        stream = '{"imu":[{"pressureValid": "%i", "pressure": "%d", "temperatureValid": "%i", "temperature": "%d", "pitch": "%d", "roll": "%d", "yaw": "%d"}]}'\
+                  % (data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"],\
+                  math.degrees(fusionPose[0]), math.degrees(fusionPose[1]), math.degrees(fusionPose[2]))
+        yield from websocket.send(stream)
+        time.sleep(self.poll_interval*1.0/1000.0)
+
+  def run(self):
+    if not os.path.exists("RTIMULib.ini"):
+      print("Settings file does not exist, will be created")
+
+    self.s = RTIMU.Settings("RTIMULib")
+    self.imu = RTIMU.RTIMU(self.s)
+    self.pressure = RTIMU.RTPressure(self.s)
+
+    if (not self.imu.IMUInit()):
+      print("IMU Init Failed")
+      sys.exit(1)
+
+    if(not self.pressure.pressureInit()):
+      print("Pressure sensor Init Failed")
+      sys.exit(1)
+
+    self.imu.setSlerpPower(0.02)
+    self.imu.setGyroEnable(True)
+    self.imu.setAccelEnable(True)
+    self.imu.setCompassEnable(True)
+    self.poll_interval = self.imu.IMUGetPollInterval()
+    
+    start_server = websockets.serve(self.handler, '192.168.0.27', 7000)
+    eventloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(eventloop)
+    eventloop.run_until_complete(start_server)
+    eventloop.run_forever()
+
 
 
 if __name__ == "__main__":
   print('aeroPi server')
-  imuThread = IMUThread()
-  imuThread.daemon = True
-  gpsThread = GPSThread()
-  gpsThread.daemon = True
-  
-  #imuThread.start()
-  gpsThread.start()
-  
-  while True:
-    time.sleep(1)
+  gpsWS = GPSWebSocket()
+  imuWS = IMUWebSocket()
+  gpsWS.start()
+  imuWS.start()
 
