86b3bc8ea682e79c5a090bfb62c6f4a4f9390393
Clement de l'Hamaide <clemaez@hotmail.fr>
2016-06-16 06:51:12 +0000

    try to play with airspace

--- www/utils.php
+++ www/utils.php
@@ -1,5 +1,16 @@
 <?php
 
+function mysqliConn(){
+  $mysqli = new mysqli("127.0.0.1", "aeropi"/*usr*/, "aeropi"/*pwd*/, "aeropi"/*db*/);
+  if($mysqli->connect_errno) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . $mysqli->connect_errno . PHP_EOL;
+    echo "Debugging error: " . $mysqli->connect_error . PHP_EOL;
+    exit;
+  }
+  return $mysqli;
+}
+
 if( isset($_GET["action"])) {
   $action = $_GET["action"];
 
@@ -17,15 +28,8 @@ if( isset($_GET["action"])) {
   }
 
   if($action == "saveSettings" || $action == "loadSettings"){
-    $mysqli = new mysqli("127.0.0.1", "aeropi"/*usr*/, "aeropi"/*pwd*/, "aeropi"/*db*/);
-    $json = array();
+    $mysqli = mysqliConn();
     $id = 1;
-    if($mysqli->connect_errno) {
-      echo "Error: Unable to connect to MySQL." . PHP_EOL;
-      echo "Debugging errno: " . $mysqli->connect_errno . PHP_EOL;
-      echo "Debugging error: " . $mysqli->connect_error . PHP_EOL;
-      exit;
-    }
     $sql = "SELECT settings FROM pilot WHERE id = $id";
     if( !$result = $mysqli->query($sql) ){
       echo "Error: Our query failed to execute and here is why: \n";
@@ -46,11 +50,13 @@ if( isset($_GET["action"])) {
       $newSettings = array_replace_recursive($currentSettings, $reqSettings);
       $newSettings = json_encode($newSettings);
       $sql = "UPDATE pilot SET settings = '$newSettings' WHERE id = $id";
-      if( !$result = $mysqli->query($sql) ){
+      if( !$mysqli->query($sql) ){
         echo "Error: Our query failed to execute and here is why: \n";
         echo "Query: " . $sql . PHP_EOL;
         echo "Errno: " . $mysqli->errno . PHP_EOL;
         echo "Error: " . $mysqli->error . PHP_EOL;
+        $result->free();
+        $mysqli->close();
         exit;
       }
     }
@@ -59,7 +65,56 @@ if( isset($_GET["action"])) {
       echo $user['settings'];
     }
     $result->free();
-    mysqli_close($db);
+    $mysqli->close();
+  }
+  
+  if($action == "getAirspace"){
+    $mysqli = mysqliConn();
+    $position = json_decode($_GET['position'], true);
+    $lng = $position['lng'];
+    $lat = $position['lat'];
+    $sql = "SELECT *, AsText(geometry) FROM `airspace` WHERE MBRContains( geometry, GeomFromText('POINT($lng $lat)') )";
+    echo $sql;
+
+    if( !$result = $mysqli->query($sql) ){
+      echo "Error: Our query failed to execute and here is why: \n";
+      echo "Query: " . $sql . PHP_EOL;
+      echo "Errno: " . $mysqli->errno . PHP_EOL;
+      echo "Error: " . $mysqli->error . PHP_EOL;
+      exit;
+    }
+    if ($result->num_rows === 0) {
+      echo "No result";
+      exit;
+    }
+    
+    $airspaces = array("type"=>"FeatureCollection", "features"=>array());
+    while($airspace = $result->fetch_assoc()){
+      unset($airspace['geometry']);
+      $airspace['info'] = utf8_decode($airspace['info']);
+      $arr = array(
+        "type" => "feature",
+        "properties" => array(
+          "name" => $airspace['name'],
+          "class" => $airspace['class'],
+          "callsign" => $airspace['callsign'],
+          "verticalLimit" => array(
+            "UPPER" => null,
+            "LOWER" => null
+          )
+        ),
+        "geometry" => array(
+          "type" => "Polygon",
+          "coordinates" => null
+        )
+      );
+      $airspaces['features'][] = $airspace;
+    }
+    echo "<pre>";
+    echo json_encode($airspaces, JSON_PRETTY_PRINT);
+    echo "</pre>";
+    $result->free();
+    $mysqli->close();
   }
 }
 ?>
